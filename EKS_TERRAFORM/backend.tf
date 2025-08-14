668ed8c849299f63732550580aa4a55f734a2ec5
Swapnil Thorat <yetstetreat@gmail.com>
2025-08-14 12:49:48 +0530

    Backend remote state file S3 Bucket added

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hotstarbucket" # Replace with your actual S3 bucket name
+    bucket = "webinax" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
